6123e20e56cd607c92cf3174637596f4c3ec3c0f
gerkenv <gerkenv@gmail.com>
2018-11-12 00:30:11 +0100

    + CRD routes for `Item`;

--- routes/api/items.js
+++ routes/api/items.js
@@ -10,9 +10,38 @@ const Item = require('../../models/Item');
 // @desc   Get all items
 // @access Public
 router.get('/', (req, res) => {
-  Item.find()                       // get all items
-    .sort({ date: -1})              // sort them by date in descending order
-    .then(items => res.json(items)) // return `json` response
+  Item.find()           // get all items
+    .sort({ date: -1})  // sort them by date in descending order
+    .then(items => res.json(200, items)) // return `json` response
+});
+
+// @route  POST api/items
+// @desc   Create a new item
+// @access Public
+router.post('/', (req, res) => {
+  // create a new item
+  const newItem = new Item({
+    name : req.body.name,
+  });
+  // store an item into database
+  newItem.save()
+    .then(item => res.json(201, item))
+    .catch(err => res.status(500).json(err));
+});
+
+// @route  DELETE api/items/:id
+// @desc   Deletes an item
+// @access Public
+router.delete('/:id', (req, res) => {
+  // get an item
+  Item.findOneAndDelete({"_id": req.params.id})
+    .then((item) => {
+      if (!item) {
+        return res.status(404).send(item);
+      } else {
+        return res.status(200).send(item);
+      }
+    }).catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
