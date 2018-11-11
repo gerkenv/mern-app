30aa5cfcec0121912b64c43e055bd784300134a6
gerkenv <gerkenv@gmail.com>
2018-11-11 15:29:15 +0100

    Server is now running with mongoose and middleware;

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// Initializing express into variable app
 const app = express();
 
-// Body-parser Middleware
+// Body-parser has a piece of middleware that we need to add
 app.use(bodyParser.json());
 
-// 
+// Get mongo DB URI from configuration
+const dbURI = require('./config/keys').mongoURI;
+
+// Connect to mongo DB
+mongoose.connect(dbURI)
+  .then(() => {
+    console.log("Database connection is established.")
+  }).catch((err) => {
+    console.err("Database connection is not established:", err);
+  });
+
+// Set a port
+// heroku stores port in this environment variable
+const port = process.env.PORT || 5000;
+
+// create server instance
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
