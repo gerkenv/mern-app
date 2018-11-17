c33773b2483ceed4c58d8e481b3b9d4ba12beb03
gerkenv <gerkenv@gmail.com>
2018-11-17 12:14:39 +0100

    +/- bugs; + comment;

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -3,16 +3,16 @@ import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
 
 const initialState = {
   items: [
-    items: [
-      { id: uuid(), name: 'Bananas' },
-      { id: uuid(), name: 'Lemon' },
-      { id: uuid(), name: 'Pineapple' },
-      { id: uuid(), name: 'Paprika' }
-    ]
+    { id: uuid(), name: 'Bananas' },
+    { id: uuid(), name: 'Lemon' },
+    { id: uuid(), name: 'Pineapple' },
+    { id: uuid(), name: 'Paprika' }
   ]
 };
 
-export default functionn(state = initialState, action) {
+// `state` argument here is not a complete store, but only a slice of it
+// associated with reducer, that dispatches an action
+export default function(state = initialState, action) {
   switch(action.type) {
     case GET_ITEMS:
       return {
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '/reducers'
+import rootReducer from './reducers'
 // equivalent to
-// import rootReducer from '/reducers/index.js'
+// import rootReducer from './reducers/index.js'
 
 const initialState = {};
 
@@ -13,6 +13,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
+    // following line is required for `Redux Dev Tools`
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
