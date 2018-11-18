b58f038512cb454bd59926b7b47df19c9fb50ef4
gerkenv <gerkenv@gmail.com>
2018-11-18 19:08:18 +0100

    + redux devtools do not play well with vs code debugger.

--- client/src/store.js
+++ client/src/store.js
@@ -14,6 +14,7 @@ const store = createStore(
   compose(
     applyMiddleware(...middleware),
     // following line is required for `Redux Dev Tools`
+    // but VS code debugger is not working with it
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
