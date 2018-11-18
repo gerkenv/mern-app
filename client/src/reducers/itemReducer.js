2cc5520d43c6e4edbf7ae4798069c24d43059c28
gerkenv <gerkenv@gmail.com>
2018-11-18 19:17:05 +0100

    + handling `DELETE_ITEM` and `ADD_ITEM`.

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -10,14 +10,23 @@ const initialState = {
   ]
 };
 
-// `state` argument here is not a complete store, but only a slice of it
-// associated with reducer, that dispatches an action
+// `state` argument here is actually a complete store, but since it is the `itemReducer` then we have to make changes only in a slice of the store called `item`. Because this reducer is associated with the `item` slice of the store. It is defined in `reducers/index.js`.
 export default function(state = initialState, action) {
   switch(action.type) {
     case GET_ITEMS:
       return {
         ...state
       };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload.id)
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload.item, ...state.items]
+      }
     default:
       return state;
   }
