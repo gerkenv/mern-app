6171838ae2f1b25c9874e588938b9c790c58a08d
gerkenv <gerkenv@gmail.com>
2018-11-19 00:08:11 +0100

    +/- `getItems`; + `setItemLoading`;

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,11 +1,25 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
+import axios from 'axios';
 
-export const getItems = () => {
-  // will be returned to our `itemReducer`
-  return {
-    type: GET_ITEMS,
-    // `payload` is not required in this action
-  }
+// Some actions here are connected through `connect` function in smart components (containers), this way, what they return is dispatched to all reducers.
+// Others are dispatching an action types on their own.
+
+export const getItems = () => dispatch => {
+  // dispatch (emit) an action (an event)
+  dispatch(setItemsLoading());
+  // send request to server to get all items
+  // we can use links without `host` name because we set `proxy` to `host` in our `package.json`
+  axios.get('/api/items')
+    .then(res => {
+      console.log({res});
+      // dispatch (emit) an action (an event)
+      dispatch({
+        type: GET_ITEMS,
+        payload: {
+          items: res.data
+        }
+      })
+    })
 }
 
 export const deleteItem = (id) => {
@@ -24,4 +38,11 @@ export const addItem = (item) => {
       item
     }
   };
+}
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING,
+    // `payload` is not required in this action
+  }
 }
